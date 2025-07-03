7173ca49cff6b66d1f3fae6ed4fbb702dc9b917d
ChernovKonstantin <chernov.konstantin@gmail.com>
2025-07-03 12:59:59 +0200

    Initial Commit

---
+++ HappyBirthdayTest/HappyBirthdayTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBirthdayTestApp.swift
+//  HappyBirthdayTest
+//
+//  Created by Chernov Kostiantyn on 03.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct HappyBirthdayTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
